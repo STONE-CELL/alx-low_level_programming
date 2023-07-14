97d1312c6caa80b96990dc5204057548a4f0a6c2
STONE-CELL <mohamedelkasir237682511@gmail.com>
2023-07-14 07:14:24 -0700

    hyt

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: print all alphabet letters
+ *
+ * Returns: 0 (success)
+ */
+int main(void)
+{
+	char po  = 'a';
+	while (po <= 'z')
+
+	{
+
+		putchar(po);
+		po++;
+	}
+	putchar('\n');
+
+	return (0);
+}
