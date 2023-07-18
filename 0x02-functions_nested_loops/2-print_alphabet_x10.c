746524748a0ef8fd470c3afb1d322932625a86eb
STONE-CELL <mohamedelkasir237682511@gmail.com>
2023-07-18 07:33:57 -0700

    uyt

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - Prints alphabet 10 times
+ *
+ * Return: always 0
+ */
+void print_alphabet_x10(void)
+{
+	int d;
+	int k;
+
+	for (d = 1; d <= 10; d++)
+	{
+		for (k = 97; k <= 122; k++)
+		{
+			_putchar('k');
+		}
+		_putchar('\n');
+	}
+}
