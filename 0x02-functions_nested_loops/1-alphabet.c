8c989e5bd519fe18c7f5532020aeea3ef2d2a6ab
STONE-CELL <mohamedelkasir237682511@gmail.com>
2023-07-18 20:36:16 -0700

    uyt

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,8 +1,7 @@
 #include "main.h"
 /**
- * main -Entry point
- * Description:prints alphapet in lowercase by print_alphabet
- * Return: always 0
+ * print_alphabet -  prints alphabet in lowercase
+ * Return: Always 0
  */
 void print_alphabet(void)
 {
