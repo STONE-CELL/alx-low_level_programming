8e887ea7fa51bdd18ff9a7319ba27d997603d611
STONE-CELL <mohamedelkasir237682511@gmail.com>
2023-07-25 00:14:11 -0700

    uyt

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -8,13 +8,13 @@ void rev_string(char *s)
 	int l, i;
 	char t;
 
-	for (l = 0; s[l] != '\0'; ++i)
-	       ;
+		for (l = 0; s[l] != '\0'; ++l)
+				;
 
-	for (i = 0; i < l / 2; i++)
-	{
-		t = s[i];
-		s[i] = s[l - 1 - i];
-		s[l - 1 - i] = t;
-	}
+		for (i = 0; i < l / 2; i++)
+		{
+			t = s[i];
+			s[i] = s[l - 1 - i];
+			s[l - 1 - i] = t;
+		}
 }
