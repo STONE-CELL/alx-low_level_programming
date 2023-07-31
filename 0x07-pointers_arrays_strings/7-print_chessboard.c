f7ab7ffdd94076bf3457bd311102c2f8050cffc7
STONE-CELL <mohamedelkasir237682511@gmail.com>
2023-07-31 03:26:53 -0700

    uyt

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -4,13 +4,13 @@
  * @a:first input
  * Return : always 0
  */
-void print_chessboard(char (*a)[0])
+void print_chessboard(char (*a)[8])
 {
 	int t, k;
 
-	for (t = 0; t < 0; t++)
+	for (t = 0; t < 8; t++)
 	{
-		for (k = 0; k < 0; k++)
+		for (k = 0; k < 8; k++)
 		{
 			_putchar(a[t][k]);
 		}
