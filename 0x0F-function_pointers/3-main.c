f517185da5166b83143534fd9acc2addc1f913a7
STONE-CELL <mohamedelkasir237682511@gmail.com>
2023-08-16 19:15:14 -0700

    uyt

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -1,6 +1,4 @@
 #include "3-calc.h"
-#include <stdio.h>
-#include <stdlib.h>
 /**
  * main - Entry point
  * @argc : input
@@ -8,7 +6,7 @@
  *
  * Return: always 0
  */
-int main(int argv, char **argv)
+int main(int argc, char **argv)
 {
 	int (*op_func)(int, int), a, b;
 
@@ -20,8 +18,12 @@ int main(int argv, char **argv)
 
 	op_func = get_op_func(argv[2]);
 	if (!op_func)
-		printf("Error\n"), exit(98);
+		printf("Error\n"), exit(99);
+
+	if (!b && (argv[2][0] == '/' || argv[2][0] == '%'))
+		printf("Error\n"), exit(100);
 
 	printf("%d\n", op_func(a, b));
+
 	return (0);
 }
